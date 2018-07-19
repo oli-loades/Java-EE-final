ce4e361376a1c272c39bed60afffe8db244a1e48
oli <oli.loades@gmail.com>
2018-07-19 15:59:49 +0100

    added rest endpoint

---
+++ src/main/java/com/qa/controller/rest/JAXLaunch.java
@@ -0,0 +1,9 @@
+package com.qa.controller.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class JAXLaunch extends Application {
+
+}
---
+++ src/main/java/com/qa/controller/rest/SubjectController.java
@@ -0,0 +1,67 @@
+package com.qa.controller.rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.apache.log4j.Logger;
+
+import com.qa.business.service.SubjectService;
+import com.qa.persistence.domain.Subject;
+
+@Path("/subject")
+public class SubjectController {
+	@Inject
+	private SubjectService subjectServ;
+	
+	private static final Logger LOGGER = Logger.getLogger(SubjectController.class);
+
+	public SubjectController() {
+
+	}
+
+	@Path("/json")
+	@GET
+	@Produces({ "application/json" })
+	public String getAllTrainees() {
+		LOGGER.info("subject controller get all subjects");
+		return subjectServ.getAllSubjects();
+	}
+
+	@Path("/json/{id}")
+	@GET
+	@Produces({ "application/json" })
+	public Subject findTrainee(@PathParam("id") Long id) {
+		LOGGER.info("subject controller find subject");
+		return subjectServ.getSubject(id);
+	}
+
+	@Path("/json/{id}")
+	@PUT
+	@Produces({ "application/json" })
+	public String updateSubject(@PathParam("id") Long id, String subject) {
+		LOGGER.info("subject controller update subject");
+		return subjectServ.updateSubject(id, subject);
+	}
+
+	@Path("/json/{id}")
+	@DELETE
+	@Produces({ "application/json" })
+	public String deleteSubject(@PathParam("id") Long id) {
+		LOGGER.info("subject controller delete subject");
+		return subjectServ.deleteSubject(id);
+	}
+
+	@Path("/json")
+	@POST
+	@Produces({ "application/json" })
+	public String createSubject(String subject) {
+		LOGGER.info("subject controller create new subject");
+		return subjectServ.createSubject(subject);
+	}
+}
